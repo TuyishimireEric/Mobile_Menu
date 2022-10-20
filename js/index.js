562553140b6799a5403ad07d9355e4f306ce24bc
Tuyishimire Eric~ <tuyishimireericc@gmail.com>
2022-10-20 23:20:56 +0200

    fix linter errors

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-document.getElementById('menu-bars').addEventListener(click, showMenu);
-
 function showMenu() {
   document.querySelector('.menu-bars').classList.toggle('active');
   document.querySelector('.desktop-nav').classList.toggle('active');
 }
-  
+
+document.getElementById('menu-bars').addEventListener('click', showMenu);
+document.getElementById('nav-back').addEventListener('click', showMenu);
